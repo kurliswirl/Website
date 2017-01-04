e9bc9a2cdf7e9fa0c0d7b13bdec5196a2c6f7595
kurliswirl <tasman_ingraham_forsyth@newtongwc.org>
2017-01-03 20:22:12 -0500

    favorite colors through the years

--- colors.py
+++ colors.py
@@ -1,8 +1,6 @@
-colors = ["red", "yellow", "green", "blue", "purple", "brown", "black", "white", "pink"]
-
+colors = ["red", "orange", "pink", "red", "pink", "magenta", "blue", "turquoise", "black"]
 
 i = 0
 while i < len(colors):
     print "When I was %d, my favorite color was %s" % (i, colors[i])
     i = i+1
-
